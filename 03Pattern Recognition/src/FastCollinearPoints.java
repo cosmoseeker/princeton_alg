a7bb3defaf124d2ab14574bddfaff038bc248551
cosmoseeker <cosmoseeker@163.com>
2015-10-04 15:00:30 +0800

    finally succeed to get 100
    do not remove the visited original points，when order by slope matain the
    points order use stable sort alg. Just like brute search, which ensure
    l>k>j>i. Every segment be found must be start with original point. The
    KEY is Just compare original with the min point to avoid subsegment

--- 03Pattern Recognition/src/FastCollinearPoints.java
+++ 03Pattern Recognition/src/FastCollinearPoints.java
@@ -30,61 +30,34 @@ public class FastCollinearPoints {
         int pointsNum = this.points.length;
         LineSegment[] totalSegments = new LineSegment[pointsNum * pointsNum / 2];
         int segNum = 0;
-        Point[] copyPoints = new Point[this.points.length], slopePoints = new Point[this.points.length - 1];
-        int slopeLength;
+        Point[] slopePoints = new Point[this.points.length];
         for (int i = 0; i < pointsNum; ++i) {
-            copyPoints[i] = this.points[i];
-        }
-        // loop to find segments
-        for (int i = 0; i < pointsNum; ++i) {
-            slopeLength = 0;
             Point original = this.points[i];
-            copyPoints[i] = null; // visited original
-            for (int j = 0; j < copyPoints.length; ++j) {
-                if (copyPoints[j] == null) {
-                    continue;
-                }
-                slopePoints[slopeLength++] = copyPoints[j];
+            for (int j = 0; j < pointsNum; ++j) {
+                slopePoints[j] = this.points[j];
             }
-            Arrays.sort(slopePoints, 0, slopeLength, original.slopeOrder());
+            Arrays.sort(slopePoints, original.slopeOrder());
             // find all segment through original
-            int start = 0, count = 0;
-            for (int k = 0; k < slopeLength; ++k) {
+            int start = 1, count = 0;
+            for (int k = 1; k < pointsNum; ++k) {
                 double sSlope = original.slopeTo(slopePoints[start]);
                 if (Double.compare(original.slopeTo(slopePoints[k]), sSlope) != 0
-                        || k == slopeLength - 1) {
-                    if (Double.compare(original.slopeTo(slopePoints[k]), sSlope) == 0) {
+                        || k == pointsNum - 1) {
+                    if (Double
+                            .compare(original.slopeTo(slopePoints[k]), sSlope) == 0) {
                         count++;
                     }
                     if (count >= 3) { // store one segment
                         int end = start + count - 1;
-                        Point min, max;
                         if (original.compareTo(slopePoints[start]) < 0) {
-                            min = original;
-                        } else {
-                            min = slopePoints[start];
-                        }
-                        if (original.compareTo(slopePoints[end]) > 0) {
-                            max = original;
-                        } else {
-                            max = slopePoints[end];
-                        }
-                        /*
-                         * Point[] segment = new Point[count + 1]; segment[0] =
-                         * original; for (int p = start; p < start + count; p++)
-                         * { segment[p - start + 1] = slopePoints[p]; }
-                         */
-                        if (checkExist(min, max, i)) {
-                            /* Arrays.sort(segment); */
                             /*
-                             * Point min = segment[0], max = segment[0]; for(int
-                             * s = 0; s < segment.length; ++s){ min =
-                             * min.compareTo(segment[s]) < 0 ? min : segment[s];
-                             * max = max.compareTo(segment[s]) > 0 ? max :
-                             * segment[s]; } totalSegments[segNum++] = new
-                             * LineSegment( segment[0], segment[count]);
+                             * StdOut.print(original.toString()); for(int t =
+                             * start; t <= end; ++t){ StdOut.print("->");
+                             * StdOut.print(slopePoints[t].toString()); }
+                             * StdOut.println("");
                              */
-                            totalSegments[segNum++] = new LineSegment(min, max);
+                            totalSegments[segNum++] = new LineSegment(original,
+                                    slopePoints[end]);
                         }
                     }
                     start = k;
@@ -99,19 +72,6 @@ public class FastCollinearPoints {
         for (int c = 0; c < segNum; ++c) {
             this.segments[c] = totalSegments[c];
         }
-    }
-
-    private boolean checkExist(/* Point[] segment, */Point min, Point max,
-            int currentOriginal) {
-        // TODO Auto-generated method stub
-        Point original = this.points[currentOriginal];
-        double segSlope = max.slopeTo(min);
-        for (int i = 0; i < currentOriginal; ++i) {
-            if (Double.compare(original.slopeTo(this.points[i]), segSlope) == 0) {
-                return false;
-            }
-        }
-        return true;
     }
 
     // the number of line segments
