807bbd1763edd5aa805863da615dfc4cea831232
cosmoseeker <cosmoseeker@163.com>
2015-11-21 11:33:16 +0800

    median maintenance
    a beautiful and elegent impl I think.
    But every oper need 2*logn time guarantee

--- Stanford_Alg1/src/alg1/MedianMaintaince.java
+++ Stanford_Alg1/src/alg1/MedianMaintaince.java
@@ -9,7 +9,7 @@ public class MedianMaintaince {
 
     public static void main(String[] args) {
         In in = new In(args[0]);
-        int[] arr = //{ 11, 3, 6, 9, 2, 8, 4, 10, 1, 12, 7, 5 };
+        int[] arr = // { 11, 3, 6, 9, 2, 8, 4, 10, 1, 12, 7, 5 };
         in.readAllInts();
         MinPQ<Integer> min = new MinPQ<Integer>();
         MaxPQ<Integer> max = new MaxPQ<Integer>();
@@ -17,36 +17,13 @@ public class MedianMaintaince {
         for (int i = 0; i < arr.length; ++i) {
             num = arr[i];
             if (min.size() == max.size()) {
-                if (max.size() > 0 && num < max.max()) {
-                    min.insert(max.delMax());
-                    max.insert(num);
-                } else {
-                    min.insert(num);
-                }
-                result = (result + min.min()) % MOD;
-                System.out.println(min.min());
-            } else if (min.size() > max.size()) {
-                if (num > min.min()) {
-                    max.insert(min.delMin());
-                    min.insert(num);
-                } else {
-                    max.insert(num);
-                }
-                result = (result + max.max()) % MOD;
-                System.out.println(max.max());
-            } else {
-                System.out.println("in else");
+                min.insert(num);
+                max.insert(min.delMin());
+            } else if (min.size() < max.size()) {
+                max.insert(num);
+                min.insert(max.delMax());
             }
-
-            /*
-             * if(max.size() == 0 ||num < max.max()) max.insert(num); else
-             * min.insert(num);
-             * 
-             * if(max.size() - min.size() > 1) { min.insert(max.max()); }
-             * if(min.size() > max.size()) { max.insert(min.min()); }
-             * 
-             * result += max.max();
-             */
+            result = (result + max.max()) % MOD;
         }
         System.out.println(result);
     }
